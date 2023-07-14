f45a5d147915c97276a10126973881bf1d35188b
Jiajie <84148881+Jijaie@users.noreply.github.com>
2023-07-14 14:05:04 -0400

    Code used for Table 1

--- Table1.R
+++ Table1.R
@@ -1,5 +1,4 @@
-# 1. Table 1
-## 1.1 Characteristic comparison at baseline and induction IIT
+## 1. Characteristic comparison at baseline and induction IIT
 df_base2 <- merge(x = df_base, y = Phys_sub2, by.x = c("StudyID", "Visit_new"), by.y = c("StudyID", "Visit_New"), all.x = T, all.y = F)
 norm_var <- c("Age", "Dura_Yr", "Weight", "BMI", "Waist", "ALTS", "CREAS", "HBACC", "GL01", "GL02", "GL03", "GL04", "GL05", "Initial_basal", "Initial_meal",
               "Final_basal", "Final_meal", "GL02_GL01", "GL03_GL01", "GL04_GL01", "GL05_GL01", "AUC_GLUC_INC",
@@ -13,7 +12,7 @@ rm_covsum(df_base2, covs = skew_var, maincov = "stabilization",digits = 2, pvalu
 rm_covsum(df_base2, covs = cat_var, maincov = "stabilization",digits = 2, pvalue =T, IQR = T, include_missing = T, full = FALSE )
 
 
-## 1.2 Characteristic comparison after 3-weeks of induction IIT
+## 2. Characteristic comparison after 3-weeks of induction IIT
 df_3w <- subset(df_final, Visit_new == 2)
 norm_var <- c("Weight", "BMI", "Waist", "ALTS", "CREAS", "HBACC", "GL01", "GL02", "GL03", "GL04", "GL05")
 skew_var <- c("IS_OGTTU","HOMA_IR", "ISSI_2", "INDEX_IR", "Cpep_Gluc", "ISR_Gluc")
@@ -22,7 +21,7 @@ rm_covsum(df_3w, covs = c(norm_var, skew_var), digits = 1, include_missing = T)
 rm_covsum(df_3w, covs = norm_var, maincov = "stabilization",digits = 1, pvalue =T, IQR = T, testcont = "ANOVA", include_missing = T, full = FALSE)
 rm_covsum(df_3w, covs = skew_var, maincov = "stabilization",digits = 2, pvalue =T, IQR = T, include_missing = T, full = FALSE)
 
-## 1.3 Change from baseline to 3-weeks
+## 3. Change from baseline to 3-weeks
 df_base_3w <- subset(df_final, Visit_new %in% c(1, 2))
 df_base_3w <- subset(df_base_3w, select = c(1, 2, 13:15, 19, 21:23, 25, 30, 33, 31, 32, 41, 42, 74))
 df_wide <- reshape(df_base_3w, idvar = c("StudyID", "stabilization"), timevar = "Visit_new", direction = "wide")
